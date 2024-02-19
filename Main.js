9e161d9e834a0447e4262b8b6323818133bb2f3f
Murtaza-Mufaddal <160309506+Murtaza-Mufaddal@users.noreply.github.com>
2024-02-19 15:01:44 +0530

    version 2.0

---
+++ Main.js
@@ -0,0 +1,59 @@
+console.log("created by Murtaza");
+
+
+
+let one = document.querySelector("#btn1");
+let two = document.querySelector("#btn2");
+let three = document.querySelector("#btn3");
+let  four = document.querySelector("#btn4");
+let five = document.querySelector("#btn5");
+let six = document.querySelector("#btn6");
+let seven = document.querySelector("#btn7");
+let eight = document.querySelector("#btn8");
+
+
+one.addEventListener('click', function () {
+  window.location.href = "first.html";
+  alert('you click on 3Rs')
+  
+});
+
+two.addEventListener('click', function () {
+  window.location.href = "second.html";
+  alert('you click on 9Rs')
+
+});
+
+three.addEventListener('click', function () {
+  window.location.href = "third.html";
+  alert('you click on 15Rs')
+});
+
+four.addEventListener('click', function () {
+  window.location.href = "forth.html";
+  alert('you click on 30Rs')
+});
+
+five.addEventListener('click', function () {
+  window.location.href = "fifth.html";
+  alert('you click on 45Rs')
+});
+
+six.addEventListener('click', function () {
+  window.location.href = "sixth.html";
+  alert('you click on 60Rs')
+});
+
+seven.addEventListener('click', function () {
+  window.location.href = "20_RS.html";
+  alert('you click on 20Rs')
+});
+
+eight.addEventListener('click', function () {
+  window.location.href = "RS++.html";
+  alert('you click on 20+Rs')
+});
+console.log("created by Murtaza");
+
+
+
